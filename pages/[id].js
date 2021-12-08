25fd91c462c1eedb2fb02abc8db4829c812da170
jenaecodes <californiacollege.jennifer@gmail.com>
2021-12-08 22:17:52 +0000

    initial repo commit

--- pages/[id].js
+++ pages/[id].js
@@ -26,15 +26,9 @@ export default function Entry({ itemData }) {
     {/* render details about one entity in persons.json saved in itemData*/}
       <article className="card col-6">
         <div className="card-body">
-          <h5 className="card-title">{itemData.name}</h5>
-          <h6 className="card-subtitle mb-2 text-muted">{itemData.phone}</h6>
-          <p className="card-text">{itemData.birthdate}</p>
-          <a href={'mailto:' + itemData.email} className="card-link">{itemData.email}</a>
-          <h4 className="card col-7">{itemData.person2.name}</h4>
-          <h5 className="card-title">{itemData.person2.phone}</h5>
-          <p className="card-text">{itemData.person2.birthdate}</p>
-          <a href={'mailto:' + itemData.person2.email} className="card-link">{itemData.person2.email}</a>
-          <p className="card-text">A friend</p>
+          <h5 className="card-title">{itemData.post_author}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">{itemData.post_date}</h6>
+          <p className="card-text">{itemData.post_content}</p>
         </div>
       </article>
     </Layout>
